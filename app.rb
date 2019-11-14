5af265a68f23a694cd95632489ef029b6754c28f
Wylan Werth <wylan@emrbear.com>
2019-11-14 15:53:27 -0800

    Remove reloading lines in production

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require "sinatra/json"
 require "sinatra/reloader" if development?
 require_relative 'report_filler'
 
-also_reload './report_filler.rb'
-also_reload './signature_pdf.rb'
+also_reload './report_filler.rb' if development?
+also_reload './signature_pdf.rb' if development?
 
 get '/' do
   json :status => 'ok'
 end
 
-post '/api/fill_form' do 
+post '/api/fill_form' do
   values = JSON.parse(request.body.read)
   report = ReportFiller.fill(values)
   status(report[:error].present? ? 500 : 200)
